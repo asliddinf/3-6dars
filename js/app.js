e738b3619ed27433bace13e578cc09f2747adf73
asliddin <asliddin>
2025-07-24 10:04:21 +0300

    initial commit

---
+++ js/app.js
@@ -0,0 +1,26 @@
+//   1
+// const getuserinfo = {
+//   name: "ali",
+//   age: 25,
+// };
+// console.log(getuserinfo);
+
+//  2
+// const myproduct = {
+//   price: 1000,
+ //  name: "telefon" 
+// };
+// function product {
+//    const tax = productprice * 0.12;
+//    withtax = productprice + tax;
+//    return myproduct;
+// };
+// console.log(product)
+  
+//  4 
+//    const carinfo = {
+//       model: "cobalt",
+//       year: 2022,
+//       color: "oq",
+//    };
+// console.log(carinfo)
